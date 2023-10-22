cfe34b21d85aee713f22a0e61e9fbd24449d31a2
SamruddhiDoiphode <148675074+SamruddhiDoiphode@users.noreply.github.com>
2023-10-22 15:58:14 +0530

    Create biggestnoamong3.py
    program to find greatest no among 3 greatest numbers

---
+++ biggestnoamong3.py
@@ -0,0 +1,11 @@
+A=int(input("Enter 1st number : "))
+B=int(input("Enter 2nd number : "))
+C=int(input("Enter 3rd number : "))
+
+if(A>C and A>B):
+  print("A is biggest")
+elif(B>A and B>C):
+  print("B is biggest")
+elif(C>A and C>B):
+  print("C is biggest")
+  
