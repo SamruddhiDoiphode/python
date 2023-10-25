7f048568b7da865e5a5e27252d04292b5485ecdd
SamruddhiDoiphode <148675074+SamruddhiDoiphode@users.noreply.github.com>
2023-10-25 19:30:23 +0530

    Create variables and data types day#6.py
    learn 5types of datas.

---
+++ variables and data types day#6.py
@@ -0,0 +1,9 @@
+# day#6
+# sequenced data
+list1=[8,2.3,[-4,5],["apple","banana"]]
+print(list1)
+tuple1=("parrot","sparrow"),("lion","tiger")
+print(tuple1)
+dict1={"Name":"sam","Age":"20","canVote":"true"}
+print(dict1)
+
